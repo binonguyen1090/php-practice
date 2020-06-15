b6251dd821a92f71b704f64bf2250c6f5e8954a5
Bino Nguyen <binonguyen1090@gmail.com>
2020-06-15 10:55:05 -0500

    variable

--- site.php
+++ site.php
@@ -1,14 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <?php 
-        echo("Hello World");
+
+    <?php
+
+        $name = "John";
+        $age = 35;
+        echo " My name is $name and I am $age years old";
+
+     
+
+    
+
     ?>
 
 </body>
+
 </html>
